93e3111b8f208a62b7cc61ab48f7db859039f5a5
Abot-Code <49647307+Abot-Code@users.noreply.github.com>
2019-04-15 20:55:32 +0700

    HELLO I AM ANONYMOUS :)
    make for fun ¯\_(ツ)_/¯

---
+++ prikol.bat
@@ -0,0 +1,71 @@
+@echo off
+title HI :)
+:a
+cls
+echo H
+timeout /t 1 >nul
+cls
+color 02
+echo HE
+timeout /t 1 >nul
+cls
+color 03
+echo HEL
+timeout /t 1 >nul
+cls
+color 04
+echo HELL
+timeout /t 1 >nul
+cls
+color 05
+echo HELLO
+timeout /t 1 >nul
+cls
+color 02
+echo HELLO I
+timeout /t 1 >nul
+cls
+color 04
+echo HELLO I A
+timeout /t 1 >nul
+cls
+color 05
+echo HELLO I AM
+timeout /t 1 >nul
+cls
+color 02
+echo HELLO I AM A
+timeout /t 1 >nul
+cls
+color 04
+echo HELLO I AM AN
+timeout /t 1 >nul
+cls
+color 05
+echo HELLO I AM ANO
+timeout /t 1 >nul
+cls
+color 01
+echo HELLO I AM ANON
+timeout /t 1 >nul
+cls
+color 02
+echo HELLO I AM ANONY
+timeout /t 1 >nul
+cls
+color 04
+echo HELLO I AM ANONYM
+timeout /t 1 >nul
+cls
+color 05
+echo HELLO I AM ANONYMO
+timeout /t 1 >nul
+cls
+color 03
+echo HELLO I AM ANONYMOU
+timeout /t 1 >nul
+cls
+color 02
+echo HELLO I AM ANONYMOUS
+timeout /t 1 >nul
+goto a
